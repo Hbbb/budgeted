5c0ecc53d3fc8362358afb68318c7cd8999b89d4
Harrison <hborges005@gmail.com>
2020-02-20 18:30:06 -0500

    Move bank client code to client.go

--- pkg/banks/auth.go
+++ pkg/banks/auth.go
@@ -7,40 +7,7 @@ import (
 	"github.com/plaid/plaid-go/plaid"
 )
 
-// BankClient exists
-type BankClient struct {
-	ClientID    string
-	Secret      string
-	PublicKey   string
-	plaidClient *plaid.Client
-}
-
-// NewBankClient exists
-func NewBankClient(clientID, secret, publicKey string) (*BankClient, error) {
-	clientOptions := plaid.ClientOptions{
-		ClientID:    clientID,
-		Secret:      secret,
-		PublicKey:   publicKey,
-		Environment: plaid.Development,
-		HTTPClient:  http.DefaultClient,
-	}
-
-	pc, err := plaid.NewClient(clientOptions)
-	if err != nil {
-		return nil, err
-	}
-
-	bc := &BankClient{
-		ClientID:    clientID,
-		Secret:      secret,
-		PublicKey:   publicKey,
-		plaidClient: pc,
-	}
-
-	return bc, nil
-}
-
-// ExchangePublicToken exchanges a public token for a private token
+// ExchangePublicToken exchanges a public token for a secret access token
 func (bc *BankClient) ExchangePublicToken(publicToken string) (string, error) {
 	if bc.plaidClient == nil {
 		return "", errors.New("banks: must use NewBankClient initializer before using the BankClient")
---
+++ pkg/banks/client.go
@@ -0,0 +1,36 @@
+package banks
+
+// BankClient wraps the Plaid API Client and stores the necessary parameters to create
+// instances of the plaid.Client
+type BankClient struct {
+	ClientID    string
+	Secret      string
+	PublicKey   string
+	plaidClient *plaid.Client
+}
+
+// NewBankClient initializes a BankClient and attaches a plaid.Client instance to it
+// with the correct options.
+func NewBankClient(clientID, secret, publicKey string) (*BankClient, error) {
+	clientOptions := plaid.ClientOptions{
+		ClientID:    clientID,
+		Secret:      secret,
+		PublicKey:   publicKey,
+		Environment: plaid.Development,
+		HTTPClient:  http.DefaultClient,
+	}
+
+	pc, err := plaid.NewClient(clientOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	bc := &BankClient{
+		ClientID:    clientID,
+		Secret:      secret,
+		PublicKey:   publicKey,
+		plaidClient: pc,
+	}
+
+	return bc, nil
+}
