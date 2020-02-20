ba5e0f317e5f0194f80d090f9537a77a12b64ac2
Harrison <hborges005@gmail.com>
2020-02-20 16:24:11 -0500

    Refactor into bankclient

--- pkg/banks/auth.go
+++ pkg/banks/auth.go
@@ -1,35 +1,52 @@
 package banks
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/plaid/plaid-go/plaid"
 )
 
-type AuthClient struct {
-	ClientID, Secret, PublicKey string
+// BankClient exists
+type BankClient struct {
+	ClientID    string
+	Secret      string
+	PublicKey   string
+	plaidClient *plaid.Client
 }
 
-func NewAuthClient(clientID, secret, publickey string) *AuthClient {
-	return &AuthClient{ClientID: clientID, Secret: secret, PublicKey: publickey}
-}
-
-// ExchangePublicToken exchanges a public token for a private token
-func (a *AuthClient) ExchangePublicToken(publicToken string) (string, error) {
+// NewBankClient exists
+func NewBankClient(clientID, secret, publicKey string) (*BankClient, error) {
 	clientOptions := plaid.ClientOptions{
-		ClientID:    a.ClientID,
-		Secret:      a.Secret,
-		PublicKey:   a.PublicKey,
+		ClientID:    clientID,
+		Secret:      secret,
+		PublicKey:   publicKey,
 		Environment: plaid.Development,
 		HTTPClient:  http.DefaultClient,
 	}
 
-	client, err := plaid.NewClient(clientOptions)
+	pc, err := plaid.NewClient(clientOptions)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	resp, err := client.ExchangePublicToken(publicToken)
+	bc := &BankClient{
+		ClientID:    clientID,
+		Secret:      secret,
+		PublicKey:   publicKey,
+		plaidClient: pc,
+	}
+
+	return bc, nil
+}
+
+// ExchangePublicToken exchanges a public token for a private token
+func (bc *BankClient) ExchangePublicToken(publicToken string) (string, error) {
+	if bc.plaidClient == nil {
+		return "", errors.New("banks: must use NewBankClient initializer before using the BankClient")
+	}
+
+	resp, err := bc.plaidClient.ExchangePublicToken(publicToken)
 	if err != nil {
 		return "", err
 	}
