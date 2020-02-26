bbb57833aa4706923f51bf901bf96b8f994964e3
Harrison <hborges005@gmail.com>
2020-02-26 11:35:24 -0500

    Refactor bank commands into top-level add/remove

--- cmd/add.go
+++ cmd/add.go
@@ -7,10 +7,6 @@ import (
 	"github.com/spf13/viper"
 )
 
-var bankCmd = &cobra.Command{
-	Use: "bank [add || remove]",
-}
-
 var bankAddCmd = &cobra.Command{
 	SilenceUsage: true,
 	Use:          "add [bankName] [publicToken]",
@@ -37,18 +33,6 @@ var bankAddCmd = &cobra.Command{
 	},
 }
 
-var bankRemoveCmd = &cobra.Command{
-	SilenceUsage: true,
-	Use:          "remove [bankName]",
-	Args:         cobra.ExactArgs(1),
-	Short:        "removes bank from manifest",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		bankName := args[0]
-		return banks.Remove(bankName)
-	},
-}
-
 func init() {
-	rootCmd.AddCommand(bankCmd)
-	bankCmd.AddCommand(bankAddCmd, bankRemoveCmd)
+	rootCmd.AddCommand(bankAddCmd)
 }
---
+++ cmd/remove.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"bdgt/pkg/banks"
+
+	"github.com/spf13/cobra"
+)
+
+var bankRemoveCmd = &cobra.Command{
+	SilenceUsage: true,
+	Use:          "remove [bankName]",
+	Args:         cobra.ExactArgs(1),
+	Short:        "removes bank from manifest",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		bankName := args[0]
+		return banks.Remove(bankName)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(bankRemoveCmd)
+}
