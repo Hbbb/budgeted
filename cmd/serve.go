1668fef51ea451fdc8dcb18d2f00725c014b31ec
Harrison <hborges005@gmail.com>
2020-02-27 13:40:37 -0500

    Remove public key flag from serve cmd; use config

--- cmd/serve.go
+++ cmd/serve.go
@@ -8,18 +8,13 @@ import (
 	"github.com/spf13/viper"
 )
 
-var publicKey string
-
 var serveCmd = &cobra.Command{
 	Use:   "serve",
 	Short: "start a local webserver that hosts the Plaid authentication UI",
 	Long:  ``,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		plaidEnv := viper.GetString("plaid-env")
-
-		if len(publicKey) < 1 {
-			return fmt.Errorf("missing plaid public key")
-		}
+		plaidEnv := viper.GetString("plaid_env")
+		publicKey := viper.GetString("plaid_public_key")
 
 		fmt.Println(`Open http://localhost:80 and follow the steps to get Plaid bank credentials.
 When you're finished, kill the process running this server.`)
@@ -29,15 +24,5 @@ When you're finished, kill the process running this server.`)
 }
 
 func init() {
-	viper.SetDefault("plaid_env", "development")
-
 	rootCmd.AddCommand(serveCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// serveCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	serveCmd.Flags().StringVarP(&publicKey, "key", "k", viper.GetString("plaid-public-key"), "plaid public key")
 }
