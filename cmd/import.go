927e98d18a33191cab3816d716575a619ebefa30
Harrison <hborges005@gmail.com>
2020-02-27 13:40:22 -0500

    Update import cmd to use new config

--- cmd/import.go
+++ cmd/import.go
@@ -13,9 +13,9 @@ var importCmd = &cobra.Command{
 	Use:   "import",
 	Short: "writes transactions directly to a Google Sheet",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		clientID := viper.GetString("plaid-client-id")
-		publicKey := viper.GetString("plaid-public-key")
-		secret := viper.GetString("plaid-secret")
+		clientID := viper.GetString("plaid_client_id")
+		publicKey := viper.GetString("plaid_public_key")
+		secret := viper.GetString("plaid_secret")
 
 		bankClient, err := banks.NewBankClient(clientID, publicKey, secret)
 		if err != nil {
